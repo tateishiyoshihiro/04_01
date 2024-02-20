1e0ade2d1bf1f0600c735acdc2643d52c8daede7
立石義弘 <115062998+tateishiyoshihiro@users.noreply.github.com>
2024-02-20 15:12:59 +0900

    完了

--- main.cpp
+++ main.cpp
@@ -8,7 +8,6 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 
 	// ライブラリの初期化
 	Novice::Initialize(kWindowTitle, 1280, 720);
-
 	// キー入力結果を受け取る箱
 	char keys[256] = {0};
 	char preKeys[256] = {0};
